bb969f9617905aba87406ccd8eefd503e316a85a
ikeda <ikeda@ikedashuuhei-no-iMac.local>
2019-01-05 19:42:32 +0900

    feat: UA設定をutilで共通で扱うようにした

--- py/saga/saga/command/bbs_menu.py
+++ py/saga/saga/command/bbs_menu.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask_script import Command
-import requests
 from logging import getLogger
+from saga.utils import http_get
 logger = getLogger(__name__)
 
 
@@ -15,15 +15,11 @@ class BBSMenu(Command):
         self._main()
 
     def _main(self):
+        # http get
         url = 'https://menu.5ch.net/bbsmenu.html'
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-        }
-        response = requests.get(url, headers=headers)
-        response.encoding = response.apparent_encoding  # shift-jis判定してutf8に変更
-        logger.info("url: {}".format(url))
+        response = http_get(url)
         if not response.ok:
-            logger.error("fail return http status is invalid")
             exit(1)
-        logger.info("size: {}byte".format(len(response.text)))
-        print(response.text)
+        # print(response.text)
+
+        # parse html
---
+++ py/saga/saga/utils/request.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from logging import getLogger
+import requests
+logger = getLogger(__name__)
+
+
+def http_get(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    }
+    response = requests.get(url, headers=headers)
+    response.encoding = response.apparent_encoding  # shift-jis判定してutf8に変更
+    logger.info("url: {}".format(url))
+    logger.info("size: {}byte".format(len(response.text)))
+    logger.info("status: {}".format(response.status_code))
+    return response

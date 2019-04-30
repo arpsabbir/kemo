36dcce6eda1c540af1b4521bda28677845a728c8
ikeda <ikeda@ikedashuuhei-no-iMac.local>
2019-04-30 21:24:23 +0900

    fix

--- py/saga/saga/command/pb2.py
+++ py/saga/saga/command/pb2.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask_script import Command
 from logging import getLogger
+from saga.auto_generated_python import news_pb2
+from importlib import import_module
 logger = getLogger(getLogger.__str__())
 
 
@@ -11,12 +13,23 @@ class Pb2(Command):
 
     def run(self):
         logger.info("start")
-        person = self._main()
+        person = self.gen_sample_pb2()
         print(type(person))
         print(person)
+        print("----------")
 
-    def _main(self):
-        from saga.auto_generated_python import news_pb2
+        # binaryに変更
+        b = person.SerializeToString()
+        print(b)
+        print(type(b))
+        print("----------")
+
+        # binary to instance
+        person2 = deserialize(b, 'saga.auto_generated_python.news_pb2.News')
+        print(type(person2))
+        print(person2)
+
+    def gen_sample_pb2(self):
         person = news_pb2.News()
         person.id = 1234
         person.name = "John Doe"
@@ -25,3 +38,11 @@ class Pb2(Command):
         phone.number = "555-4321"
         phone.type = news_pb2.News.HOME
         return person
+
+
+def deserialize(byte_message, proto_type):
+    module_, class_ = proto_type.rsplit('.', 1)
+    class_ = getattr(import_module(module_), class_)
+    rv = class_()
+    rv.ParseFromString(byte_message)
+    return rv
